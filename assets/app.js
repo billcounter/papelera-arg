64fb4a9fe9cfe7546431ed13f7f596ea7c3b7a2d
billcounter <billcounterargentina@gmail.com>
2026-06-27 22:41:05 -0300

    Add files via upload

---
+++ assets/app.js
@@ -0,0 +1,5 @@
+const toggle = document.querySelector('[data-menu-toggle]');
+const menu = document.querySelector('[data-menu]');
+if (toggle && menu) {
+  toggle.addEventListener('click', () => menu.classList.toggle('open'));
+}
